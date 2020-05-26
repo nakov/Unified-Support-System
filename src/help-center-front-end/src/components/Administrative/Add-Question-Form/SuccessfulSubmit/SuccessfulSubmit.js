4423ba0f2400145a71f87e3441145016c2e1467e
Kiril Kirilov <kironakumura@abv.bg>
2020-05-26 16:56:22 +0300

    Help Center [FE] - Administrative View Styling Fixes

--- src/help-center-front-end/src/components/Administrative/Add-Question-Form/SuccessfulSubmit/SuccessfulSubmit.js
+++ src/help-center-front-end/src/components/Administrative/Add-Question-Form/SuccessfulSubmit/SuccessfulSubmit.js
@@ -12,7 +12,7 @@ function SuccessfulSubmit() {
         <div class="administrative-question__main__confirmation">
             <h1>Thank you for contacting us!</h1>
             <FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon>
-            <p>Your question was received and is currently beeing processed. You will receive response by emailin the next few hours</p>
+            <p>Your question was received and is currently beeing processed. You will receive response by email in the next few hours</p>
             <a href="">
                 <FontAwesomeIcon icon={faComments}></FontAwesomeIcon>
                 <p>
--- src/help-center-front-end/src/components/Administrative/FAQ-Details/FaqDetails.js
+++ src/help-center-front-end/src/components/Administrative/FAQ-Details/FaqDetails.js
@@ -11,21 +11,21 @@ function FaqDetails(props) {
             <div className="administrative-question__main__content-wrapper">
                 <div className="administrative-question__main__question-content">
                     <p>Q</p>
-                    <p>{selectedQuestion.content}</p>
+                    <p className="administrative-question__main__question-content__question">{selectedQuestion.content}</p>
                 </div>
                 <div className="administrative-question__main__answer-content">
                     <p className="border">A</p>
-                    <div>
-                        <p>During the New Year's holiday our technical support team will work with limited
+                    <div className="administrative-question__main__answer-content__answers-container">
+                        <p className="administrative-question__main__answer-content__answer">During the New Year's holiday our technical support team will work with limited
                         resources. so you can expect answers to technical questions within 48 hours.</p>
-                        <p>Note: questions in English are usually answered faster than questions in your local
+                        <p className="administrative-question__main__answer-content__answer">Note: questions in English are usually answered faster than questions in your local
                         language.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
+                        <p className="administrative-question__main__answer-content__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
+                        <p className="administrative-question__main__answer-content__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
+                        <p className="administrative-question__main__answer-content__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
+                        <p className="administrative-question__main__answer-content__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
+                        <p className="administrative-question__main__answer-content__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
+                        <p className="administrative-question__main__answer-content__answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem deserunt possimus accusamus! Eum eveniet iure earum illo recusandae, dignissimos quis? Quasi non totam fuga velit! Voluptatum reprehenderit atque et?</p>
                     </div>
                 </div>
             </div>
