23c231cea53ee808a018af6a663f29e809e63028
Boogy83 <bozhidar.atanasov@abv.bg>
2020-06-10 11:01:32 +0300

    component addressbar added

---
+++ src/help-center-front-end/src/components/Address-bar/AddressBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './AddressBar.css';
+
+function AddressBar() {
+
+    const text = 'dynamically given text';
+   
+    return (
+        <div className="help-center__container__address-bar">{text}</div>
+    )
+}
+
+export default AddressBar;
